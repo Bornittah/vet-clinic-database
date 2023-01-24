8d90f707bf666155ac7890a9ab2ef8757b01808e
bornittah <bornittah@gmail.com>
2023-01-24 23:50:41 +0300

    Create database and table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
+create database vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+    id SERIAL PRIMARY KEY,
+    name text NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal NOT NULL
 );
